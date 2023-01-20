6842f8529ac40f02cfdfcc0da2513957d1a6dcc1
brayan sigala <brayansigala21@gmail.com>
2023-01-20 11:01:49 -0700

    segundo  commit

--- counter.js
+++ counter.js
@@ -2,7 +2,7 @@ const array1 = [1,2,3,4,56,7,8,10]
 const array2 = [1,1,1,2,10]
 
 const funcionSuma = (array) =>{
-  const arrayEach =  array.forEach(  )
+  const arrayEach =  array.forEach( puta )
 
     console.log(arrayEach)
    
